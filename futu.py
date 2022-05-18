875a0f23f94c0e64bf3c4db4a2de7e20973cc955
Marc Zhu <a0983204766@gmail.com>
2022-05-18 20:26:01 +0800

    test2

--- futu.py
+++ futu.py
@@ -8,7 +8,6 @@ def futu_news():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH',None), chrome_options=chrome_options)
-    driver = webdriver.Chrome(chromedriver)
     driver.get('https://news.futunn.com/hk/main?lang=zh-hk')
     driver.maximize_window()
     titles = driver.find_elements_by_class_name('news-title')[:10]
@@ -20,8 +19,12 @@ def futu_news():
     return str(news)
 
 def futu_news_us():
-    chromedriver = 'chromedriver.exe'
-    driver = webdriver.Chrome(chromedriver)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN',None)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH',None), chrome_options=chrome_options)
     driver.get('https://news.futunn.com/hk/main?lang=en-US')
     driver.maximize_window()
     titles = driver.find_elements_by_class_name('news-title')[:10]
